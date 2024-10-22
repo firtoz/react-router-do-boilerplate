b9c33145c7ba5c129926bf7d1c96ad267a7b9475
firtoz <108406948+firtoz@users.noreply.github.com>
2024-10-22 12:07:43 +0100

    It works!!

--- durable-objects/example-do/src/ExampleDO.ts
+++ durable-objects/example-do/src/ExampleDO.ts
@@ -3,7 +3,7 @@ import type { DOWithHonoApp } from "@greybox/hono-typed-fetcher/honoDoFetcher";
 import { Hono } from "hono";
 
 export class ExampleDO extends DurableObject implements DOWithHonoApp {
-	app = new Hono().get("/", (c) => c.text("Hello World"));
+	app = new Hono().get("/", (c) => c.text("Hello World!"));
 
 	constructor(ctx: DurableObjectState, env: unknown) {
 		console.log("ExampleDO constructor");
